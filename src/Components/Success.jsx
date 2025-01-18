ec66d4633b11601669ec90153b14d1ac2c244f6d
Sabariks092 <sabariks092@gmail.com>
2025-01-18 14:27:57 +0530

    small update on success page

--- src/Components/Success.jsx
+++ src/Components/Success.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { AppContext } from '../Context/AppContext';
 
 export const Success = () => {
+  const {user}= useContext(AppContext);
 
   const navigate = useNavigate();
   const onclick=()=>{
@@ -10,8 +12,17 @@ export const Success = () => {
   return (
     <div className='text-white mt-5 text-center' style={{height:"80vh"}}>
       <h2>Currently unavailable :(</h2>
-      <p>please login from another account</p>
-      <button onClick={onclick} className='promtBtn rounded-5' style={{width:"100px"}}>Signup</button>
+      {!user &&
+        <div>
+          <p>please login from another account</p>
+          <button onClick={onclick} className='promtBtn rounded-5' style={{width:"100px"}}>Signup</button>
+        </div> }
+
+        {user &&
+        <div>
+          ...
+        </div>
+        }
     </div>
   )
 }
